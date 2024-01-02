908e56a73759484780cd5d9fc040e9c2f11789cb
Charlie Dauber <charlie.dauber@hotmail.com>
2024-01-02 16:39:16 +0000

    I have managed to make the grid interactive now, when creating a user you will be randomly allocated a gamepiece, then clicking the buttons on the grid will show the gamepiece. It will then wipe when you get 3 in a row and update the score. Need to add a function to stop at 3 and also see how to create an opposing player

--- script.js
+++ script.js
@@ -3,49 +3,47 @@ form.addEventListener("submit", function(event) {
     event.preventDefault(); // Prevents the form from submitting and the page from reloading 
   });
 
-const name = "";
-const gamepiece = 0;
+
+let player = document.getElementById("nameInput");
 const gameboard = [];
 
-function createPlayer(name, gamepiece){
-    name = document.getElementById("nameInput");
-    gamepiece =  Math.floor(Math.random()*2);
+function createPlayer(name){
+    name = player;
+    let score = 0;
+    let gamepiece =  Math.floor(Math.random()*2);
     if(gamepiece == 0){
         gamepiece = "X";
-    } else if(gamepiece = 1){
+    } else if(gamepiece == 1){
         gamepiece = "O";
     }
-    console.log(name.value + " is playing with " + gamepiece);
-    return {name, gamepiece};
+    //console.log(name.value + " is playing with " + gamepiece);
+    const showScore = ()  => console.log(score);
+    const roundWon = () => score++;
+    //const showGamepiece = () => gamepiece;
+    return {name, gamepiece, showScore , roundWon};
 }
+
+let user = createPlayer(player)
 
 function gameBoard(){
 let grid = document.querySelector(".gameboard");
 grid.innerHTML = "";
     for(let y=0; y<3; y++){
         gameboard[y] = []
-        //let //gridCellY = document.createElement("div");
-            //gridCellY.id = "gridCellY" + y;
-            //gridCellY.className = "gridCell";
-            //gridCellY.innerHTML=`
-            //<div id="gridContent${y} class= "gridContent">test</div>
-            //`
-            //grid.appendChild(gridCellY);
-        
         for(let x=0; x<3; x++){
             addCell(y, x);
             let gridCellX = document.createElement("div");
             gridCellX.id = "gridCellX"+ x;
             gridCellX.className = "gridCell";
             gridCellX.innerHTML=`
-            <div id="gridContent${x} class= "gridContent">test</div>
+            <div id="gridContent${x}${y}" class= "gridContent"><button onclick="playInput(${x}, ${y})"></button></div>
             `
         grid.appendChild(gridCellX);
         }
     }
 console.log(gameboard);
-playGame(gameboard)
-return gameboard;
+playGame(gameboard);
+//return gameboard;
 }
 
 function addCell(x, y){
@@ -53,27 +51,57 @@ function addCell(x, y){
 }
 
 function cell(x,y){
-    return "X";
+    return "";
 }
 
 function playGame(gameboard){
-if(gameboard[0][0] && gameboard[1][0] && gameboard[2][0] === 'X'){
+if(gameboard[0][0] && gameboard[1][0] && gameboard[2][0] === 'X' || gameboard[0][0] && gameboard[1][0] && gameboard[2][0] === 'O'){
     console.log('Winner')
-}  else if(gameboard[0][1] && gameboard[1][1] && gameboard[2][1] === 'X'){
+    gameBoard();
+    user.roundWon();
+    user.showScore();
+}  else if(gameboard[0][1] && gameboard[1][1] && gameboard[2][1] === 'X' || gameboard[0][1] && gameboard[1][1] && gameboard[2][1] ==='O'){
     console.log('Winner')
-} else if(gameboard[0][2] && gameboard[1][2] && gameboard[2][2] === 'X'){
+    gameBoard();
+    user.roundWon();
+    user.showScore();
+} else if(gameboard[0][2] && gameboard[1][2] && gameboard[2][2] === 'X' || gameboard[0][2] && gameboard[1][2] && gameboard[2][2] ==='O'){
     console.log('Winner')
-}  else if(gameboard[0][0] && gameboard[0][1] && gameboard[0][2] === 'X'){
+    gameBoard();
+    user.roundWon();
+    user.showScore();
+}  else if(gameboard[0][0] && gameboard[0][1] && gameboard[0][2] === 'X' || gameboard[0][0] && gameboard[0][1] && gameboard[0][2] ==='O' ){
     console.log('Winner')
-}  else if(gameboard[1][0] && gameboard[1][1] && gameboard[1][2] === 'X'){
+    gameBoard();
+    user.roundWon();
+    user.showScore();
+}  else if(gameboard[1][0] && gameboard[1][1] && gameboard[1][2] === 'X' || gameboard[1][0] && gameboard[1][1] && gameboard[1][2] ==='O' ){
     console.log('Winner')
-} else if(gameboard[2][0] && gameboard[2][1] && gameboard[2][2] === 'X'){
+    gameBoard();
+    user.roundWon();
+    user.showScore();
+} else if(gameboard[2][0] && gameboard[2][1] && gameboard[2][2] === 'X' || gameboard[2][0] && gameboard[2][1] && gameboard[2][2] === 'O'){
     console.log('Winner')
-}  else if(gameboard[0][0] && gameboard[1][1] && gameboard[2][2] === 'X'){
+    gameBoard();
+    user.roundWon();
+    user.showScore();
+}  else if(gameboard[0][0] && gameboard[1][1] && gameboard[2][2] === 'X' || gameboard[0][0] && gameboard[1][1] && gameboard[2][2] === 'O' ){
     console.log('Winner')
-}  else if(gameboard[2][0] && gameboard[1][1] && gameboard[0][2] === 'X'){
+    gameBoard();
+    user.roundWon();
+    user.showScore();
+}  else if(gameboard[2][0] && gameboard[1][1] && gameboard[0][2] === 'X' || gameboard[2][0] && gameboard[1][1] && gameboard[0][2] === 'O'){
     console.log('Winner')
-} else{
-    console.log('Loser')
+    gameBoard();
+    user.roundWon();
+    user.showScore();
 }
+}
+
+
+function playInput(x, y) {
+    let select = document.getElementById("gridContent"+x+y);
+    select.innerHTML = user.gamepiece;
+    gameboard[y][x] = user.gamepiece;
+    playGame(gameboard)
 }
